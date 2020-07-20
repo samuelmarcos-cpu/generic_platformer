0524cddc7ca9353f1df1f2bdf8326e21bd39fe25
Marcos Samuel <m.samuel43.yahoo@gmail.com>
2020-07-19 22:19:25 -0300

    tomanokusigraça

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: {
+    game: './game/app.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  }
+}
